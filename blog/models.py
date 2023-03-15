49e5c6ab199b5509cd93f3dfc68386e2e36408da
dla9944 <dla9944@naver.com>
2023-03-15 16:45:56 +0900

    블로그_포스팅_대표이미지_파일,일반_파일_업로드_기능_추가

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
     def __str__(self):
         return f'[{self.pk}]{self.title}'
 
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 import mysql_settings
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +126,8 @@ USE_TZ = False
 
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
