0e758023b89ff315c81441a1e07f3c0a4529e97e
dla9944 <dla9944@naver.com>
2023-03-16 10:48:25 +0900

    230316 hook text와 본문 미리보기 기능 추가(NVIDIA tech 홈페이지 모방중)

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank = True)
     content = models.TextField()
     # created_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add = True)
@@ -15,6 +17,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
     #author 추후 작성
 # Create your models here.
